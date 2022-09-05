f50b0a9ade0977dfb5dfbdd56d6682d3493079c3
Maria <mary-sberbank@yandex.ru>
2022-09-05 19:33:13 +0300

    change like/dislike card

--- backend/controllers/cardsController.js
+++ backend/controllers/cardsController.js
@@ -72,7 +72,7 @@ module.exports.likeCard = (req, res, next) => {
       throw new NotFoundError('Карточка с указанным id не найдена');
     })
     .then((card) => {
-      res.send({ data: card });
+      res.send(card);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -97,7 +97,7 @@ module.exports.dislikeCard = (req, res, next) => {
       throw new NotFoundError('Карточка с указанным id не найдена');
     })
     .then((card) => {
-      res.send({ data: card });
+      res.send(card);
     })
     .catch((err) => {
       if (err.name === 'CastError') {
