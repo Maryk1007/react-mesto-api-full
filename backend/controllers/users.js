5d256f2ef714d2a2ad628a1c7286846c02a09540
Maria <mary-sberbank@yandex.ru>
2022-09-06 01:38:15 +0300

    wesfrfyyt

---
+++ backend/controllers/users.js
@@ -0,0 +1,173 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const CastError = require('../errors/cast-error');
+const NotFoundError = require('../errors/not-found-error');
+const ConflictError = require('../errors/conflict-error');
+const { SALT_ROUNDS, MONGO_DUPLICATE_ERROR_CODE } = require('../utils/constants');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+/** добавить пользователя */
+module.exports.createUser = (req, res, next) => {
+  const {
+    email,
+    password,
+    name,
+    about,
+    avatar,
+  } = req.body;
+  return bcrypt
+    .hash(password, SALT_ROUNDS)
+    .then((hash) => (
+      User
+        .create({
+          email,
+          password: hash,
+          name,
+          about,
+          avatar,
+        })
+    ))
+    .then((user) => {
+      res.send({
+        email: user.email,
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+      });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        next(new CastError('Введены некорректные данные пользователя'));
+      } else if (err.code === MONGO_DUPLICATE_ERROR_CODE) {
+        next(new ConflictError('Пользователь с указанным email уже существует'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+/** получить всех пользователей */
+module.exports.getUsers = (req, res, next) => {
+  User
+    .find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch(next);
+};
+
+/** получить информацию о пользователе */
+module.exports.getMe = (req, res, next) => {
+  const userId = req.user._id;
+  User
+    .findById(userId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь по указанному id не найден');
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new CastError('Введен некорректный id пользователя'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+/** получить пользователя по ID */
+module.exports.getUserById = (req, res, next) => {
+  const { userId } = req.params;
+  User
+    .findById(userId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь по указанному id не найден');
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new CastError('Введен некорректный id пользователя'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+/** обновить данные пользователя */
+module.exports.updateUser = (req, res, next) => {
+  const { name, about } = req.body;
+  const userId = req.user._id;
+  User
+    .findByIdAndUpdate(
+      userId,
+      { name, about },
+      { new: true, runValidators: true },
+    )
+    .orFail(() => {
+      const err = new Error('Пользователь по указанному id не найден');
+      err.name = 'NotFoundError';
+      throw err;
+    })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'NotFoundError') {
+        next(new NotFoundError('Пользователь по указанному id не найден'));
+      } else if (err.name === 'ValidationError' || err.name === 'CastError') {
+        next(new CastError('Введены некорректные данные пользователя'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+/** обновить аватар пользователя */
+module.exports.updateAvatar = (req, res, next) => {
+  const { avatar } = req.body;
+  const userId = req.user._id;
+  User
+    .findByIdAndUpdate(
+      userId,
+      { avatar },
+      { new: true, runValidators: true },
+    )
+    .orFail(() => {
+      const err = new Error('Пользователь по указанному id не найден');
+      err.name = 'NotFoundError';
+      throw err;
+    })
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'NotFoundError') {
+        next(new NotFoundError('Пользователь по указанному id не найден'));
+      } else if (err.name === 'ValidationError' || err.name === 'CastError') {
+        next(new CastError('Введены некорректные данные пользователя'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+/** аутентификация - вход по email и паролю  */
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User
+    .findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+      res.send({ token });
+    })
+    .catch(next);
+};
